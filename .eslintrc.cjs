393840e50abc12041e69c56b2d2089112a6dcbc9
LarbeartoW <larbeartow@gmail.com>
2024-07-19 04:24:21 -0700

    Update ESLint rules and Prisma schema for safer coding practices and data management.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -29,10 +29,13 @@ const config = {
     'tailwindcss/classnames-order': 'off',
     '@typescript-eslint/no-unsafe-member-access': 'off',
     '@typescript-eslint/no-unsafe-assignment': 'off',
-
+    '@typescript-eslint/no-unsafe-call': 'off',
+    '@typescript-eslint/no-unsafe-argument': 'off',
+    '@typescript-eslint/ban-ts-comment': 'off',
     '@typescript-eslint/prefer-nullish-coalescing': 'off',
     '@typescript-eslint/array-type': 'off',
     '@typescript-eslint/consistent-type-definitions': 'off',
+    '@typescript-eslint/prefer-optional-chain': 'off',
     '@typescript-eslint/consistent-type-imports': [
       'warn',
       {
